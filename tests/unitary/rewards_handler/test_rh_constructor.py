35caba6bd5a6e2884bde4cbfded7ea47054677b9
Alberto <albicento.ac@gmail.com>
2024-09-23 15:45:31 +0200

    test: rewards handler constructor

---
+++ tests/unitary/rewards_handler/test_rh_constructor.py
@@ -0,0 +1,28 @@
+import boa
+import pytest
+
+
+@pytest.fixture(scope="module")
+def rewards_handler_deployer():
+    return boa.load_partial("contracts/RewardsHandler.vy")
+
+
+def test_default_behavior(
+    rewards_handler_deployer, crvusd, vault, minimum_weight, mock_controller_factory, curve_dao
+):
+    rewards_handler = rewards_handler_deployer(
+        crvusd, vault, minimum_weight, mock_controller_factory, curve_dao
+    )
+
+    assert rewards_handler._immutables.stablecoin == crvusd.address
+    assert rewards_handler._immutables.vault == vault.address
+    assert rewards_handler.vault() == vault.address
+    assert rewards_handler.minimum_weight() == minimum_weight
+    assert rewards_handler._storage.minimum_weight.get() == minimum_weight
+    assert rewards_handler.eval("lens.factory.address") == mock_controller_factory.address
+    assert rewards_handler.hasRole(rewards_handler.DEFAULT_ADMIN_ROLE(), curve_dao)
+    assert rewards_handler.hasRole(rewards_handler.RATE_MANAGER(), curve_dao)
+    # eoa would be the deployer from which we revoke the role
+    assert not rewards_handler.hasRole(rewards_handler.DEFAULT_ADMIN_ROLE(), boa.env.eoa)
+    assert rewards_handler.eval("twa.twa_window") == 86400 * 7
+    assert rewards_handler.eval("twa.min_snapshot_dt_seconds") == 1
