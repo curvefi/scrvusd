f5d94723f71c711ebab34fecece16758689ca974
Alberto <albicento.ac@gmail.com>
2024-10-08 11:42:34 +0200

    test: make deposit test account rewards

--- tests/unitary/yearn/test_deposit.py
+++ tests/unitary/yearn/test_deposit.py
@@ -1,7 +1,7 @@
 import boa
 
 
-def test_deposit(vault, crvusd, role_manager, vault_god):
+def test_deposit(vault, crvusd, role_manager, vault_god, rewards_handler, curve_dao):
     alice = boa.env.generate_address()
     boa.deal(crvusd, alice, 10**21)
 
@@ -14,9 +14,18 @@ def test_deposit(vault, crvusd, role_manager, vault_god):
     assert vault.balanceOf(alice) == 10**21
     assert crvusd.balanceOf(alice) == 0
 
-    with boa.env.prank(alice):
-        vault.approve(vault, 10**21, sender=alice)
-        vault.redeem(10**21, alice, alice, sender=alice)
+    print(rewards_handler.distribution_time())
+    boa.deal(crvusd, rewards_handler, 10**21)
+    rewards_handler.set_distribution_time(86400 * 7, sender=curve_dao)
+    rewards_handler.process_rewards()
+    # we make 3 days pass
 
-    assert vault.balanceOf(alice) == 0
-    assert crvusd.balanceOf(alice) == 10**21
+    for i in range(7):
+        boa.env.time_travel(86400 * i)
+        with boa.env.anchor(), boa.env.prank(alice):
+            vault.approve(vault, 10**21, sender=alice)
+            vault.redeem(10**21, alice, alice, sender=alice)
+            assert vault.balanceOf(alice) == 0
+            assert crvusd.balanceOf(alice) >= 10**21
+            print("alice balance has increased")
+            print(crvusd.balanceOf(alice))
