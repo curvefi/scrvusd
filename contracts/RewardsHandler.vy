35e373d5d868f422a4435aa25e63b601532fc042
Alberto <albicento.ac@gmail.com>
2024-10-09 11:06:49 +0200

    Revert "chore: downgrade vault (#18)"
    This reverts commit 92b278d5885088423381ffb986ec45f4a8579a5f.

--- contracts/RewardsHandler.vy
+++ contracts/RewardsHandler.vy
@@ -298,7 +298,8 @@ def set_distribution_time(new_distribution_time: uint256):
     extcall vault.setProfitMaxUnlockTime(new_distribution_time)
 
     # enact the changes
-    extcall vault.process_report(staticcall vault.default_queue(0))
+    extcall vault.process_report(vault.address)
+
 
 @external
 def set_minimum_weight(new_minimum_weight: uint256):
--- tests/unitary/conftest.py
+++ tests/unitary/conftest.py
@@ -1,5 +1,4 @@
 import boa
-import boa_solidity  # noqa: F401
 import pytest
 
 MOCK_CRV_USD_CIRCULATING_SUPPLY = 69_420_000 * 10**18
@@ -46,21 +45,12 @@ def role_manager():
 
 
 @pytest.fixture(scope="module")
-def vault(vault_factory, crvusd, role_manager, dummy_strategy):
+def vault(vault_factory, crvusd, role_manager):
     vault_deployer = boa.load_partial("contracts/yearn/Vault.vy")
 
     address = vault_factory.deploy_new_vault(crvusd, "Staked crvUSD", "st-crvUSD", role_manager, 0)
 
-    vault = vault_deployer.at(address)
-
-    # creata a local god to avoid recursive dependencies in fixtures
-    _god = boa.env.generate_address()
-
-    vault.set_role(_god, int("11111111111111", 2), sender=role_manager)
-
-    vault.add_strategy(dummy_strategy, sender=_god)  # TODO figure out queue
-
-    return vault
+    return vault_deployer.at(address)
 
 
 @pytest.fixture(scope="module")
@@ -116,7 +106,6 @@ def mock_peg_keeper():
 def rewards_handler(
     vault, crvusd, role_manager, minimum_weight, scaling_factor, mock_controller_factory, curve_dao
 ):
-    print(crvusd, vault, minimum_weight, scaling_factor, mock_controller_factory, curve_dao)
     rh = boa.load(
         "contracts/RewardsHandler.vy",
         crvusd,
@@ -126,30 +115,7 @@ def rewards_handler(
         mock_controller_factory,
         curve_dao,
     )
+
     vault.set_role(rh, 2**11 | 2**5 | 2**0, sender=role_manager)
 
     return rh
-
-
-@pytest.fixture(scope="module")
-def solc_args():
-    return {
-        "optimize": True,
-        "optimize_runs": 200,
-    }
-
-
-@pytest.fixture(scope="module")
-def tokenized_strategy(solc_args, vault_factory):
-    deployer = boa.load_partial_solc(
-        "contracts/yearn/TokenizedStrategy.sol", compiler_args=solc_args
-    )
-    return deployer.deploy(
-        vault_factory.address, override_address="0x2e234DAe75C793f67A35089C9d99245E1C58470b"
-    )
-
-
-@pytest.fixture(scope="module")
-def dummy_strategy(solc_args, crvusd, tokenized_strategy):
-    deployer = boa.load_partial_solc("contracts/yearn/DummyStrategy.sol", compiler_args=solc_args)
-    return deployer.deploy(crvusd.address, "dummy")
