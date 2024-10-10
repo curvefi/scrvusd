cda4d79c8f9b07383c2c6e72cfde32764b2d77ee
Michael <151919150+heswithme@users.noreply.github.com>
2024-10-10 13:36:34 +0200

    chore: use ruff (#24)
    * chore: ruff
    
    * chore: bump pre-commit, redo ci
    
    * chore: bump pre-commit, redo ci

--- tests/unitary/deposit_limit_module/test_set_deposits_paused.py
+++ tests/unitary/deposit_limit_module/test_set_deposits_paused.py
@@ -1,7 +1,10 @@
 import boa
 
 
-def test_default_behavior(deposit_limit_module, dev_address):
+def test_default_behavior(
+    deposit_limit_module,
+    dev_address,
+):
     # Pause deposits using the security_agent privileges of dev_address
     with boa.env.prank(dev_address):
         deposit_limit_module.set_deposits_paused(True)
@@ -17,12 +20,18 @@ def test_default_behavior(deposit_limit_module, dev_address):
     assert not deposit_limit_module.deposits_paused()
 
 
-def test_set_deposits_paused_unauthorized(deposit_limit_module, security_agent):
+def test_set_deposits_paused_unauthorized(
+    deposit_limit_module,
+    security_agent,
+):
     # Verify deposits are initially unpaused
     assert not deposit_limit_module.deposits_paused()
 
     # Attempt to pause deposits using an unauthorized address
-    with boa.env.prank(security_agent), boa.reverts("Caller is not a security_agent"):
+    with (
+        boa.env.prank(security_agent),
+        boa.reverts("Caller is not a security_agent"),
+    ):
         deposit_limit_module.set_deposits_paused(True)
 
     # Verify that deposits are still unpaused since the call should have failed
--- tests/unitary/rewards_handler/test_implements_interface.py
+++ tests/unitary/rewards_handler/test_implements_interface.py
@@ -1,7 +1,7 @@
 SUPPORTED_INTERFACES = [
-    b"\x01\xFF\xC9\xA7",  # ERC165
-    b"\x79\x65\xDB\x0B",  # access_control.vy
-    b"\xA1\xAA\xB3\x3F",  # dynamic weight
+    b"\x01\xff\xc9\xa7",  # ERC165
+    b"\x79\x65\xdb\x0b",  # access_control.vy
+    b"\xa1\xaa\xb3\x3f",  # dynamic weight
 ]
 
 
