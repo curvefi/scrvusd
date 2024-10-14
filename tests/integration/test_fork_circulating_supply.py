4f34794dd793920bec374ca7117579bb1b7c5036
Alberto <albicento.ac@gmail.com>
2024-10-14 12:11:54 +0200

    test: removed redundant test

--- tests/integration/test_fork_circulating_supply.py
+++
@@ -1,5 +0,0 @@
-def test_fork_circulating_supply(lens):
-    # TODO mint more crvusd and make sure this increases accordingly
-    # TODO increase flashloan debt ceiling and make
-    #  sure supply doesn't increase
-    print("{:2e}".format(lens.internal._circulating_supply()))
